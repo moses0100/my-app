6e920308bd0b1e3efd230dc8d0c6f7f63032ab6c
moses0100 <tanawatyadjantuek@gmail.com>
2026-06-29 16:15:21 +0700

    update

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>เกี่ยวกับเรา</title>
+</head>
+
+<body>
+    <h2>เกี่ยวกับเรา</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad cum unde temporibus corporis non nam tempora maxime
+        incidunt, vero numquam, sed ex impedit ut. Totam, sed tempore. Nisi, autem aut!</p>
+    <a href="/">หน้าแรก</a>
+</body>
+
+</html>
